bab24837f7fb6590346f4e55f0b523547a8960d8
graeme benson <62527977+g-r-a-e-m-e@users.noreply.github.com>
2023-12-10 16:42:46 -0500

    Create clean.py
    TODO - test results

---
+++ python/exploratory/ECO2LSTE-LST-LST-err-2023-12-10-v0-1/clean.py
@@ -0,0 +1,6 @@
+# Import packages
+import pandas as pd
+import os
+
+# Specify path
+path = './data/exploratory/ECO2LSTE-LST-LST-err-2023-12-10-v0-1/ECO2LSTE-LST-LST-err-2023-12-10-v0-1-ECO2LSTE-001-results.csv'
